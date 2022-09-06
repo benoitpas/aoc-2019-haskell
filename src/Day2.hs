e46a1d07aab2290c64019d487db995314163d7e8
benoitpas <bpasquereau@gmail.com>
2022-09-06 08:47:04 +0100

    added type signature for next step

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,6 +1,8 @@
 module Day2
-    ( run, wordsWhen
+    ( run, wordsWhen, str2array
     ) where
+
+import Data.Array
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
@@ -8,8 +10,16 @@ wordsWhen p s =  case dropWhile p s of
                       s' -> w : wordsWhen p s''
                             where (w, s'') = break p s'
 
+str2array:: String -> Array Int Int
+str2array s = 
+    let strings = map read (wordsWhen (==',') s) in
+    listArray (0,(length strings) - 1) strings
+
+nextStep :: ((Array Int Int),Int) -> ((Array Int Int),Int)
+nextStep (program,index) = (program ,index)
+
 run :: IO ()
 run = do
     content <- readFile "src/day2_input.txt"
-    let program = (wordsWhen (==',') (head (lines content)))
-    putStrLn (head program)
+    let program = str2array (head (lines content))
+    print program
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -1,7 +1,11 @@
 module Day2Spec (spec) where
 
 import Test.Hspec
+import Data.Array
 import Day2
+
+p1 :: Array Int Int
+p1 = str2array "1,9,10,3,2,3,11,0,99,30,40,50"
 
 spec :: Spec
 spec = do
