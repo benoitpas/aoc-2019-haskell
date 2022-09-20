ef4b22ae6875cc14e901f043e22d569d6cf5b9d3
benoitpas <bpasquereau@gmail.com>
2022-09-20 07:14:29 +0100

    support for more opcodes

--- src/Day5.hs
+++ src/Day5.hs
@@ -16,14 +16,30 @@ data State = State {
     output:: Maybe Int
 }
 
+instance Show State where
+  show (State a b c d) = "{" ++ show a ++ "," ++ show b ++  "," ++ show c ++ "," ++ show d ++ "}"
+
+instance Eq State where
+  x == y = (memory x == memory y && ip x == ip y && input x == input y && output x == output y)
+
+
 nextStep::  State ->  State
 nextStep state =
     let m = memory state
         i = ip state 
-    in case m ! ip state of
+    in case (m ! i) `mod` 10 of
+        1  -> process (+)
+        2  -> process (*)
         3 -> state { memory = m // [(m ! (i + 1), input state)], ip = i + 2 }
         4 -> state { output = Just (m ! (m ! (i + 1))), ip = i + 2 }
         _ -> state { ip = -i - 1 }
+    where
+        process op =
+            let m = memory state
+                i = ip state in
+            let a = if (m ! i `div` 100)  `mod` 10 == 1 then m ! (i + 1) else m ! (m ! (i + 1))
+                b = if (m ! i `div` 1000) `mod` 10 == 1 then m ! (i + 2) else m ! (m ! (i + 2)) in
+                state { memory = (m // [(m ! (i + 3), a `op` b)]), ip = (i + 4)}
 
 allSteps :: State -> State
 allSteps state = last $ takeWhile (\state -> ip state >= 0) (iterate nextStep state)
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -1,20 +1,19 @@
 module Day5Spec (spec) where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck (modifyMaxSuccess,prop)
+import Test.Hspec.QuickCheck (prop)
 
 import Day2 (list2array)
 import Day5
 
-instance Show State where
-  show (State a b c d) = "{" ++ show a ++ "," ++ show b ++  "," ++ show c ++ "," ++ show d ++ "}"
+s1 :: State
+s1 = State {memory = list2array [3,0,4,0,99], ip = 0, input = 42, output = Nothing}
 
-instance Eq State where
-  x == y = (memory x == memory y && ip x == ip y && input x == input y && output x == output y)
+s2 :: State
+s2 = State {memory = list2array [42,0,4,0,99], ip = 2, input = 42, output = Nothing}
 
-s1 = State {memory = list2array [3,0,4,0,99], ip= 0,input=42,output= Nothing}
-s2 = State {memory = list2array [42,0,4,0,99], ip = 2, input= 42,output= Nothing}
-s3 = State {memory = list2array [42,0,4,0,99], ip = 4, input= 42,output= Just 42}
+s3 :: State
+s3 = State {memory = list2array [42,0,4,0,99], ip = 4, input = 42, output = Just 42}
 
 spec :: Spec
 spec = do
@@ -24,6 +23,17 @@ spec = do
                 (nextStep s1) `shouldBe` s2
         it "check writing to input" $ do
                 (nextStep s2) `shouldBe` s3
+        it "ends the program" $ do
+            (nextStep (State (list2array [99]) 0 0 Nothing)) `shouldBe` (State (list2array [99]) (-1) 0 Nothing)
+        it "processes an addition" $ do
+            (nextStep (State (list2array [1,0,0,0]) 0 0 Nothing)) `shouldBe` (State (list2array [2,0,0,0]) 4 0 Nothing)
+        it "processes an addition with immediate access" $ do
+            (nextStep (State (list2array [1101,100,-1,4,0]) 0 0 Nothing)) `shouldBe` (State (list2array [1101,100,-1,4,99]) 4 0 Nothing)
+        it "processes a multiplication" $ do
+            (nextStep (State (list2array [2,3,0,3,99]) 0 0 Nothing)) `shouldBe` (State (list2array [2,3,0,6,99]) 4 0 Nothing)
+        it "processes a multiplication with immediate access" $ do
+            (nextStep (State (list2array [1002,4,3,4,33]) 0 0 Nothing)) `shouldBe` (State (list2array [1002,4,3,4,99]) 4 0 Nothing)
+
 
     describe "runProgram" $ do
         prop "simple input/output test" $
