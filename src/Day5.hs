cc337b740f9cbf12e5234d8b0626f95f2ba23344
benoitpas <bpasquereau@gmail.com>
2022-09-26 06:35:37 +0100

    refactoring to prepare Day 7

--- src/Day5.hs
+++ src/Day5.hs
@@ -12,7 +12,7 @@ import Day2  (str2array)
 data State = State {
     memory :: Array Int Int,
     ip :: Int,
-    input :: Int,
+    input :: [Int],
     output:: Maybe Int
 }
 
@@ -27,24 +27,24 @@ nextStep::  State ->  State
 nextStep state =
     let m = memory state
         i = ip state in
-        let param ip inc = if (m ! ip `div` (if inc == 1 then 100 else 1000) `mod` 10) == 1 then m ! (ip + inc) else m ! (m ! (ip + inc))
-            process op = state { memory = (m // [(m ! (i + 3), (param i 1) `op` (param i 2))]), ip = (i + 4)}
-            compare op = state { memory = m // [(m ! (i + 3), if (param i 1) `op` (param i 2) then 1 else 0)], ip = (i + 4)}
+        let param inc = if (m ! i `div` (if inc == 1 then 100 else 1000) `mod` 10) == 1 then m ! (i + inc) else m ! (m ! (i + inc))
+            process op = state { memory = (m // [(m ! (i + 3), (param 1) `op` (param 2))]), ip = (i + 4)}
+            comp op = state { memory = m // [(m ! (i + 3), if (param 1) `op` (param 2) then 1 else 0)], ip = (i + 4)}
         in case (m ! i) `mod` 10 of
             1  -> process (+)
             2  -> process (*)
-            3 -> state { memory = m // [(m ! (i + 1), input state)], ip = i + 2 }
-            4 -> state { output = Just (param i 1), ip = i + 2 }
-            5 -> state { ip = if (param i 1) > 0 then param i 2 else i + 3 }
-            6 -> state { ip = if (param i 1) == 0 then param i 2 else i + 3 }
-            7 -> compare (<)
-            8 -> compare (==)
+            3 -> state { memory = m // [(m ! (i + 1), head (input state))], ip = i + 2 , input = tail (input state)  }
+            4 -> state { output = Just (param 1), ip = i + 2 }
+            5 -> state { ip = if (param 1) > 0 then param 2 else i + 3 }
+            6 -> state { ip = if (param 1) == 0 then param 2 else i + 3 }
+            7 -> comp (<)
+            8 -> comp (==)
             _ -> state { ip = -i - 1 }
                
 allSteps :: State -> State
-allSteps state = last $ takeWhile (\state -> ip state >= 0) (iterate nextStep state)
+allSteps iState = last $ takeWhile (\state -> ip state >= 0) (iterate nextStep iState)
 
-runProgram :: String -> Int -> Maybe Int
+runProgram :: String -> [Int] -> Maybe Int
 runProgram program i =
     let iState = State { memory = str2array program, ip = 0, input = i, output = Nothing }
     in
@@ -55,7 +55,7 @@ run :: IO ()
 run = do
     content <- readFile "src/day5_input.txt"
     let program = (head (lines content))
-    let p1 = runProgram program 12
+    let p1 = runProgram program [12]
     print ("puzzle 1: " ++ show p1)
-    let p2 = runProgram program 5
+    let p2 = runProgram program [5]
     print ("puzzle 2: " ++ show p2)
