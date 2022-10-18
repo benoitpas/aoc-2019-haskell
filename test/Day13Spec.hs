4a7e245ca82e2e4dc3673e8c845a184ceda36ae2
benoitpas <bpasquereau@gmail.com>
2022-10-18 18:56:30 +0100

    forgot to fix the tests

--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -8,4 +8,4 @@ spec :: Spec
 spec = do
     describe "toTiles" $ do
         it "extract the tiles" $ do
-            toTiles [1,2,3,6,5,4] `shouldBe` [((1,2),Paddle),((6,5),Ball)]
+            toTiles [1,2,3,6,5,4] `shouldBe` [(Paddle (1,2)),(Ball (6,5))]
